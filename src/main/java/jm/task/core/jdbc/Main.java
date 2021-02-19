b814adbf735f8a40d1b97dcf172929ae489d578e
levgenix <mr.lobach@gmail.com>
2021-02-19 12:07:21 +0300

    Complete task without atabase.properties

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+/**
+ *  Создание таблицы User(ов)
+ *  Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в
+ *  консоль ( User с именем – name добавлен в базу данных )
+ *  Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+ *  Очистка таблицы User(ов)
+ *  Удаление таблицы
+ */
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Джо", "Байден", (byte) 78);
+        userService.saveUser("Трамп", "Дональд", (byte) 74);
+        userService.saveUser("Барак", "Обама", (byte) 59);
+        userService.saveUser("Джордж", "Буш", (byte) 74);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String urlJDBC = "jdbc:mysql://localhost:3306/taskjdbc?useSSL=false";
+    private static final String userName = "root";
+    private static final String userPassword = "root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(urlJDBC, userName, userPassword);
+    }
 }
